81611bc953650077262fd9f2ca38ce438fe40ec6
Jesse Hernandez <jesseovr@gmail.com>
2025-07-24 22:02:40 -0500

    review vv poster

--- app/components/PortfolioGrid.tsx
+++ app/components/PortfolioGrid.tsx
@@ -472,9 +472,9 @@ export const portfolioArray: PortfolioItem[] = [
     description:
       "A bilingual space for those who aren’t afraid to question, create, and grow.",
     video: "https://ampd-asset.s3.us-east-2.amazonaws.com/VV_Intro_V01.mp4",
-    poster: "https://ampd-asset.s3.us-east-2.amazonaws.com/vv-poster.png",
-    photo: "https://ampd-asset.s3.us-east-2.amazonaws.com/vv-poster.png",
-    gif: "https://ampd-asset.s3.us-east-2.amazonaws.com/vv-poster.png",
+    poster: "https://ampd-asset.s3.us-east-2.amazonaws.com/vemosvamos-poster.png",
+    photo: "https://ampd-asset.s3.us-east-2.amazonaws.com/vemosvamos-poster.png",
+    gif: "https://ampd-asset.s3.us-east-2.amazonaws.com/vemosvamos-poster.png",
     showImage: true,
     showVideo: true,
     content:
